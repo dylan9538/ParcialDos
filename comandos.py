dce1e577bd9528e6af641227e36891c1ab9d8af9
Dylan Torres <diland.t95@hotmail.com>
2016-11-06 12:22:10 -0500

    Add files via upload

---
+++ comandos.py
@@ -0,0 +1,21 @@
+from subprocess import Popen, PIPE
+
+def get_all_files():
+  grep_process = Popen(["ls"], stdout=PIPE, stderr=PIPE)
+  file_list = Popen(["awk","-F","/",'{print $1}'], stdin=grep_process.stdout, stdout=PIPE,stderr=PIPE).communicate()[0].split('\n')
+  return filter(None,file_list) 
+
+def add_file(filename,content):
+  add_process = Open(filename+'.txt','a')
+  add_process.write(content+'/n')
+  add_process.close()
+  return "Se creo el archivo" , 201 
+
+def remove_file(filename):
+  vip = ["ambientes","repositories","comandos","comandosTest"]
+  if filename in vip:
+    return True
+  else:
+    remove_process = Popen(["rm","-r",filename], stdout=PIPE, stderr=PIPE)
+    remove_process.wait()
+    return False if filename in get_all_files() else True
---
+++ comandosTest.py
@@ -0,0 +1,47 @@
+from comandos import *
+
+def addTest():
+
+  fileName = "fileAddTest"
+  contenido = "soy el archivo de add test"
+
+  archivosIniciales = get_all_files()
+  numFilesUno = len(archivosIniciales)
+
+  add_file(fileName,contenido)
+  archivosNuevos = get_all_files(0)
+  numFilesDos = len(archivosNuevos)
+ 
+  numEsperado = numFilesUno + 1
+
+  assert numEsperado == numFilesDos,"El numero de archivos actual deberia ser igual al numFilesUno mas uno"
+
+def deleteTest():
+  fileName = "fileAddTest.txt"
+
+  archivosActuales = get_all_files()
+  numFilesUno = len(archivosActuales)
+
+  remove_file(fileName)
+  archivosNuevos  = get_all_files()
+  numFilesDos = len(archivosNuevos)
+ 
+  numEsperado = numFilesUno - 1
+
+  assert numEsperado  == numFilesDos,"El numero de archivos deberia ser igual a numFilesUno menos uno"
+
+def getAllTest():
+   
+   primeraCantidad = len(get_all_files());
+   
+   add_file("fileOne","soy el archivo one");
+   add_file("fileTwo","soy el archivo dos");
+   add_file("fileThree,"soy el archivo tres");
+   segundaCantidad = len(get_all_files());
+  
+   remove_file("fileOne.txt");
+   remove_file("fileTwo.txt");
+   remove_file("fileThree.txt");
+   tercerCantidad = len (get_all_files());
+
+   assert primeraCantidad == tercerCantidad and primeraCantidad + 3 == segundaCantidad
